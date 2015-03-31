6372f77d99a1a068baf01f18fdcb43823288ddcb
Arthur <arthur.p.wright@gmail.com>
2015-03-31 13:40:20 -0400

    Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+# 1. Calculating a tip
+puts 55 * 0.20
+
+# 2. Adding a string and an integer
+puts "I'm number " + 1.to_s
+
+# 3. Interpolation
+puts "45628 multiplied by 7839 equals #{45628 * 7839}"
+
+# 4. Truthiness
+puts (true && false) || (false && true) || !(false && false)
